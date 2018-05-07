fb9a8ccf65a9f8de8929b0a3716e3e31f211b173
Sean Clifford <sclifford6262@gmail.com>
2018-05-07 20:17:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ class Route {
     }
 
     estimatedTime(peak = false){
-      peak ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3
-    )
+    return   peak ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3
+    }
 
 }
