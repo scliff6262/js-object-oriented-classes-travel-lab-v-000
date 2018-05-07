a9c6a0ec60a601f6f5c63adfc4cab56bad9b6b10
Sean Clifford <sclifford6262@gmail.com>
2018-05-07 19:58:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,30 +18,30 @@ class Route {
   convertToNumbers(obj){
     let numberValues = {}
     switch(obj.horizontal){
-      case horizontal.includes('1st'):
+      case obj.horizontal.includes('1st'):
         numberValues["horizontal"] = 1
         break;
-      case horizontal.includes('2nd'):
+      case obj.horizontal.includes('2nd'):
         numberValues["horizontal"] = 2
         break;
-      case horizontal.includes('3rd'):
+      case obj.horizontal.includes('3rd'):
         numberValues["horizontal"] = 3
         break;
-      case horizontal.includes('Lexington'):
+      case obj.horizontal.includes('Lexington'):
         numberValues["horizontal"] = 4
         break;
-      case horizontal.includes('Park'):
+      case obj.horizontal.includes('Park'):
         numberValues["horizontal"] = 5
         break;
-      case horizontal.includes('Madison'):
+      case obj.horizontal.includes('Madison'):
         numberValues["horizontal"] = 6
         break;
-      case horizontal.includes('5th'):
+      case obj.horizontal.includes('5th'):
         numberValues["horizontal"] = 7
         break;
     }
     numberValues["vertical"] = parseInt(obj.vertical)
   }
-  
+
   blocksTravelled()
 }
